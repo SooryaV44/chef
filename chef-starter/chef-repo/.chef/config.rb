3bd856485b91447a4b163d6aed6a44b88b95ed37
SooryaV44 <123071295+SooryaV44@users.noreply.github.com>
2024-11-19 11:45:46 +0530

    Update config.rb

--- chef-starter/chef-repo/.chef/config.rb
+++ chef-starter/chef-repo/.chef/config.rb
@@ -3,7 +3,7 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "sooryav"
+node_name                "chef-node"
 client_key               "#{current_dir}/sooryav.pem"
 chef_server_url          "https://api.chef.io/organizations/chefserver02"
 cookbook_path            ["#{current_dir}/../cookbooks"]
